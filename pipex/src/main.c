35250d0a2a4375c08d72c90a0a65db2693bf1ee0
jrocha-v <jrocha-v@student.42porto.com>
2023-09-07 15:21:11 +0100

    update

--- pipex/src/main.c
+++ pipex/src/main.c
@@ -6,7 +6,7 @@
 /*   By: jrocha-v <jrocha-v@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 10:14:52 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/09/07 13:21:48 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/09/07 15:12:16 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,22 @@
 
 int main(int argc, char **argv, char **envp) 
 {
-	char 	**clean_argv;
+	//char 	**clean_argv;
 	char	**clean_envp;
-	char	*cmd;
+	char	**cmd;
 	char	*cmd_path;
 	int 	i;
 
 	i = 0;
-	if (argc != 3)
+	if (argc != 2)
 	{
 		printf("Bad arg usage");
 		return (1);
 	}
 	
 	clean_envp = ft_parse_envp(envp);
-	cmd = get_first_word(clean_argv[0]);
-	cmd_path = get_cmd_path(clean_envp, cmd);
+	cmd = ft_split(argv[1], ' '); // alterar para 1
+	cmd_path = get_cmd_path(clean_envp, cmd[0]);
 	
 	i = 0;
 	while (clean_envp[i])
