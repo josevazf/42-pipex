9f95a7f487cfd8f92d10cecab06e979c4b369453
jrocha-v <jrocha-v@student.42porto.com>
2023-09-18 17:03:23 +0100

    update

--- pipex/includes/pipex.h
+++ pipex/includes/pipex.h
@@ -6,17 +6,18 @@
 /*   By: jrocha-v <jrocha-v@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 09:49:31 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/09/07 13:06:37 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/09/08 15:29:25 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include "../libft/libft.h"
-# include <unistd.h>
-# include <stdlib.h>
-# include <stdio.h>
+#include "../libft/libft.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
 
 // utils_argv.c
 char	**ft_parse_envp(char **envp); 					// OK
--- pipex/src/main.c
+++ pipex/src/main.c
@@ -6,40 +6,63 @@
 /*   By: jrocha-v <jrocha-v@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 10:14:52 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/09/07 15:12:16 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/09/08 16:03:19 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int main(int argc, char **argv, char **envp) 
+void	parent(int *fd_pipe, char **argv, char **clean_envp)
 {
-	//char 	**clean_argv;
+	int 	fd_file;
+	char	*file;
+	
+	close(fd_pipe[0]); // Closing pipe Read end 
+	file = argv[1];
+	
+}
+
+int 	main(int argc, char **argv, char **envp) 
+{
 	char	**clean_envp;
-	char	**cmd;
-	char	*cmd_path;
+	//char	**cmd;
+	//char	*cmd_path;
+	int		fd_pipe[2];
+	int		pid;
 	int 	i;
 
+
 	i = 0;
-	if (argc != 2)
+	clean_envp = ft_parse_envp(envp);
+	if (argc != 5)
 	{
 		printf("Bad arg usage");
 		return (1);
 	}
+	if (pipe(fd_pipe) == -1)
+		return (1);
+	pid = fork();
+	if (pid == -1);
+		return (1);
+	if (pid == 0)
+		child(fd_pipe, argv, clean_envp);
+	wait(NULL);
+	parent(fd_pipe, argv, clean_envp);
+	return (0);
+/* 	cmd = ft_split(argv[1], ' '); // alterar para 1
+	cmd_path = get_cmd_path(clean_envp, cmd[0]); */
 	
-	clean_envp = ft_parse_envp(envp);
-	cmd = ft_split(argv[1], ' '); // alterar para 1
-	cmd_path = get_cmd_path(clean_envp, cmd[0]);
+
+
 	
-	i = 0;
+/* 	i = 0;
 	while (clean_envp[i])
 	{
 		printf("envp[%d]: %s\n", i, clean_envp[i]);
 		i++;
 	}
-
 	if (execve(cmd_path, cmd, envp) == -1)
 	{
 		return (2);
-	}
+	} */
 }
